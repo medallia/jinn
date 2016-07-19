f180b97c363f3174651f9842a913bcb4b54164e9
JC Martin <jcmartin@medallia.com>
2016-07-19 11:46:01 -0300

    empty commit

--- get_servers.rb
+++ get_servers.rb
@@ -1,6 +1,6 @@
 require 'ipaddr'
 
-def check_count(role_name, c, fd)
+def check_count(role_name, c)
   if c.count == nil
     raise Vagrant::Errors::VagrantError.new,"Incompatible constraints: #{role_name}: Missing count"
   end
@@ -24,22 +24,37 @@ def check_ru(role_name, c, fd)
   end
 end
 
+def allocate_random(subnet, id, fd_map)
+  ip = nil
+  tries = 0
+  begin
+    ip = subnet.to_range.to_a[1..-1].sample()
+    tries = tries+1
+  end while (fd_map[id].include?(ip.to_s) || tries < 5)
+  if tries == 5 
+    return nil
+  else
+    return ip
+  end
+end
+
 def get_server(fault_domains, fd_map, role_name, c)
   ip = nil
-  fault_domains.each do |f|
+  fault_domains.each do |fd|
 
-    subnet = IPAddr.new(f['subnet'])
+    subnet = IPAddr.new(fd['subnet'])
+    id = fd['id']
 
-    if fd_map[f['id']] == nil
-      fd_map[f['id']] = Array.new
+    if fd_map[id] == nil
+      fd_map[id] = Array.new
     end
     # is there a fd choice
-    if c.fd_choice != nil && c.fd_choice != f['id']
+    if c.fd_choice != nil && c.fd_choice != id
       next
     end
 
     # is the limit for that fd reached
-    if c.limit != nil && (fd_map[f['id']].size >= c.limit)
+    if c.limit != nil && (fd_map[id].size >= c.limit)
       next
     end
 
@@ -47,22 +62,18 @@ def get_server(fault_domains, fd_map, role_name, c)
     if c.ru != nil 
       # calculate IP based on RU based numbering
       ip = subnet | (c.ru.to_i*4)+2
-      if fd_map[f['id']].include?(ip.to_s)
+      if fd_map[id].include?(ip.to_s)
         next
       end
     else
-      tries=0
-      begin
-        ip = subnet.to_range.to_a[1..-1].sample()
-        tries = tries+1
-      end while (fd_map[f['id']].include?(ip.to_s) || tries < 5)
-      if tries == 5 
+      ip = allocate_random(subnet, id, fd_map)
+      if ip == nil
         raise Vagrant::Errors::VagrantError.new,"Cannot allocate IP: #{role_name}: too many tries"
       end
     end
 
     if ip != nil
-      fd_map[f['id']] << ip.to_s
+      fd_map[id] << ip.to_s
       break
     end
   end
@@ -74,7 +85,7 @@ def get_servers(role_name, role, fault_domains)
 
   c = Constraints.new(role_constraints['count'], role_constraints['limit'], role_constraints['fd'], role_constraints['unit'] )
 
-  check_count(role_name, c, fault_domains)
+  check_count(role_name, c)
   check_fd(role_name, c, fault_domains)
   check_ru(role_name, c, fault_domains)
 
